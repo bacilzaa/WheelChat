d6bd63a272c600b4b2fe445a981648c489819162
= <basinnaja@hotmail.com>
2021-12-02 21:36:20 +0700

    set directory

---
+++ app/src/main/java/com/juniverse/wheelchat/ui/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.juniverse.wheelchat.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.juniverse.wheelchat.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
