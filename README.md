ace6a2f2fcc5c4e4361f142b5a9726809d782334
= <basinnaja@hotmail.com>
2021-12-02 19:59:46 +0700

    set design pattern MVVM

---
+++ README.md
@@ -0,0 +1,1 @@
+# wheelchat application
--- app/src/main/java/com/juniverse/wheelchat/ui/activity/MainActivity.kt
+++ app/src/main/java/com/juniverse/wheelchat/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.juniverse.wheelchat
+package com.juniverse.wheelchat.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.juniverse.wheelchat.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
